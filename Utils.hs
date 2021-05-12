e92c1144e9468787ac5e3dc0f37afe4a3c2deade
Lautaro Emanuel <emlautarom1@gmail.com>
2021-05-12 18:43:39 -0300

    Common used functions

---
+++ Utils.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Utils where
+
+indexOf :: Eq a => a -> [a] -> Maybe Int
+indexOf e l = go (zip [0 ..] l)
+  where
+    go [] = Nothing
+    go ((idx, x) : xs) = if e == x then Just idx else go xs
+
+replaceAt :: Int -> a -> [a] -> [a]
+replaceAt 0 rep (_ : xs) = rep : xs
+replaceAt idx rep (x : xs) = x : replaceAt (idx - 1) rep xs
+replaceAt _ _ [] = []
+
+replaceWhen :: (a -> Bool) -> a -> [a] -> [a]
+replaceWhen p rep = map (\e -> if p e then rep else e)
+
+chunks :: Int -> [a] -> [[a]]
+chunks _ [] = []
+chunks n xs = let (ys, zs) = splitAt n xs in ys : chunks n zs
