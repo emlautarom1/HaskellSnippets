d13e29cf0717379454d642f9bf9f26e2d5a46a25
Lautaro Emanuel <emlautarom1@gmail.com>
2021-05-12 19:32:47 -0300

    Simplified implementation code

--- PropertySet.hs
+++ PropertySet.hs
@@ -36,13 +36,12 @@ where
 type Predicate a = a -> Bool
 
 data Operation a
-  = Insert (Predicate a) (Set a)
+  = Noop
+  | Insert (Predicate a) (Set a)
   | Delete (Predicate a) (Set a)
 
 -- | A set of values @a@.
-data Set a
-  = EmptySet
-  | PropSet (Operation a)
+data Set a = MkSet (Operation a)
 
 instance Semigroup (Set a) where
   (<>) = union
@@ -50,19 +49,18 @@ instance Semigroup (Set a) where
 instance Monoid (Set a) where
   mempty = empty
 
-evalPredicate :: Set a -> Predicate a
-evalPredicate EmptySet = const False
-evalPredicate (PropSet op) = case op of
-  Insert predicate s -> \x -> x `member` s || predicate x
-  Delete predicate s -> \x -> x `member` s && (not $ predicate x)
+evalOp :: Operation a -> Predicate a
+evalOp Noop = const False
+evalOp (Insert predicate s) = \x -> x `member` s || predicate x
+evalOp (Delete predicate s) = \x -> x `member` s && (not $ predicate x)
 
 -- | The empty set.
 empty :: Set a
-empty = EmptySet
+empty = MkSet Noop
 
 -- | Is a given element in the set?
 member :: a -> Set a -> Bool
-member a s = (evalPredicate s) a
+member a (MkSet op) = evalOp op a
 
 -- | Is a given element __not__ in the set?
 notMember :: a -> Set a -> Bool
@@ -72,23 +70,23 @@ notMember a s = not $ member a s
 -- Insertion of an element already on the set is a no-op.
 insert :: Eq a => a -> Set a -> Set a
 insert a s
-  | notMember a s = PropSet $ Insert (== a) s
+  | notMember a s = MkSet $ Insert (== a) s
   | otherwise = s
 
 -- | Insert all the elements that satisfy a @Predicate@ in a set.
 insertAll :: (Predicate a) -> Set a -> Set a
-insertAll p s = PropSet $ Insert p s
+insertAll p s = MkSet $ Insert p s
 
 -- | Delete a single element from a set.
 -- Deletion of an element not in the set is a no-op.
 delete :: Eq a => a -> Set a -> Set a
 delete a s
-  | member a s = PropSet $ Delete (== a) s
+  | member a s = MkSet $ Delete (== a) s
   | otherwise = s
 
 -- | Delete all the elements that satisfy a given @Predicate@ in a set.
 deleteAll :: (Predicate a) -> Set a -> Set a
-deleteAll p s = PropSet $ Delete p s
+deleteAll p s = MkSet $ Delete p s
 
 -- | Create a set with a single element.
 singleton :: Eq a => a -> Set a
@@ -104,15 +102,15 @@ fromPredicate p = insertAll p empty
 
 -- | Union of two sets.
 union :: Set a -> Set a -> Set a
-union s1 s2 = PropSet $ Insert (`member` s1) s2
+union s1 s2 = MkSet $ Insert (`member` s1) s2
 
 -- | Difference of two sets.
 difference :: Set a -> Set a -> Set a
-difference s1 s2 = PropSet $ Insert (`notMember` s2) s1
+difference s1 s2 = MkSet $ Insert (`notMember` s2) s1
 
 -- | Intersection of two sets.
 intersection :: Set a -> Set a -> Set a
-intersection s1 s2 = PropSet $ Insert (\x -> x `member` s1 && x `member` s2) empty
+intersection s1 s2 = MkSet $ Insert (\x -> x `member` s1 && x `member` s2) empty
 
 -- | See 'difference'
 (\\) :: Set a -> Set a -> Set a
