bca124760656080352568bee3e57bc769f304532
Lautaro Emanuel <emlautarom1@gmail.com>
2021-05-12 18:41:07 -0300

    Coding challenge
    Can't find the original source but it's probably from Leetcode/HackerRank

---
+++ UnivalTrees.hs
@@ -0,0 +1,38 @@
+module UnivalTrees
+  ( Tree (..),
+    isUnival,
+    univalCount,
+  )
+where
+
+data Tree a
+  = Empty -- Creates an empty node, a "leaf" of a Tree
+  | Node a (Tree a) (Tree a)
+  -- Creates a Tree from a Node with 2 children:
+  -- a left and a right, and both are also a Tree
+  deriving (Show, Eq)
+
+isUnival :: Eq a => Tree a -> Bool
+isUnival tree = case tree of
+  Empty -> False
+  Node v l r -> case (l, r) of
+    (Node lv _ _, Node rv _ _) ->
+      v == lv && v == rv && isUnival l && isUnival r
+    (Node lv _ _, Empty) -> v == lv && isUnival l
+    (Empty, Node rv _ _) -> v == rv && isUnival r
+    (Empty, Empty) -> True
+
+-- |
+-- * Test cases
+-- >>> univalCount $ Node 1 (Node 1 Empty Empty) Empty
+-- 2
+-- >>> univalCount $ Node 0 (Node 1 Empty Empty) Empty
+-- 1
+-- >>> univalCount $ Node 1 (Node 1 Empty (Node 0 Empty Empty)) Empty
+-- 1
+univalCount :: Eq a => Tree a -> Int
+univalCount tree = case tree of
+  Empty -> 0
+  Node _ l r -> if isUnival tree then 1 + childCount else childCount
+    where
+      childCount = univalCount l + univalCount r
