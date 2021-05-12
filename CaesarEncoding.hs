75cac164edd8766ca095afcaf835716f3f086878
Lautaro Emanuel <emlautarom1@gmail.com>
2021-05-12 18:37:48 -0300

    Primitive caesar encoding
    Used for a r/haskell comment

---
+++ CaesarEncoding.hs
@@ -0,0 +1,30 @@
+module Caesar
+  ( encodeCaesar,
+    decodeCaesar,
+  )
+where
+
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+
+alphabet :: [Char]
+alphabet = ['a' .. 'z'] ++ ['A' .. 'Z']
+
+alphabetCharIndex :: Char -> Int
+alphabetCharIndex chr = fromJust $ elemIndex chr alphabet
+
+encodeCaesar :: String -> Int -> String
+encodeCaesar input offset = map (\chr -> cycle alphabet !! encodedIndex chr) input
+  where
+    encodedIndex chr = alphabetCharIndex chr + offset
+
+decodeCaesar :: String -> Int -> String
+decodeCaesar input offset = map (\chr -> cycle alphabet !! decodedIndex chr) input
+  where
+    decodedIndex chr
+      | idx < 0 = alphabetLength + idx
+      | otherwise = idx
+      where
+        alphabetLength = length alphabet
+        idx = alphabetCharIndex chr - realOffset
+        realOffset = offset `mod` alphabetLength
