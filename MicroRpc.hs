d2735f869bf9942431ced2df184f3cd4ff5ce3d3
Lautaro Emanuel <emlautarom1@gmail.com>
2024-03-07 11:48:02 -0300

    Support IO methods

--- MicroRpc.hs
+++ MicroRpc.hs
@@ -1,4 +1,7 @@
+module MicroRpc (main) where
+
 import Control.Monad (forM_)
+import Data.IORef (atomicModifyIORef', newIORef)
 import Data.Map (Map)
 import Data.Map qualified as M
 import Data.Typeable (Proxy (..), TypeRep, Typeable, typeRep)
@@ -8,15 +11,16 @@ import Text.Read (readMaybe)
 -- Lib
 
 data Method = Method
-  { f :: String -> Maybe String
+  { f :: String -> Maybe (IO String)
   , expectedArgs :: TypeRep
   }
 
-mkMethod :: forall i o. (Typeable i, Read i, Show o) => (i -> o) -> Method
+mkMethod :: forall i o. (Typeable i, Read i, Show o) => (i -> IO o) -> Method
 mkMethod f =
   Method
     { f = \s -> case readMaybe s of
-        Just args -> Just $ show $ f args
+        Just args -> do
+          Just $ show <$> f args
         Nothing -> Nothing
     , expectedArgs = typeRep (Proxy :: Proxy i)
     }
@@ -29,13 +33,14 @@ instance Show RpcError where
   show (MismatchedArgs ty) = "Mismatched types, expected `" <> show ty <> "`"
   show (MethodNotFound name) = "Method `" <> name <> "` not found"
 
-runRpc :: Map String Method -> (String, String) -> Either RpcError String
+runRpc :: Map String Method -> (String, String) -> IO (Either RpcError String)
 runRpc methods (method, args) = do
-  Method func ty <- M.lookup method methods !? MethodNotFound method
-  func args !? MismatchedArgs ty
-
-(!?) :: Maybe a -> e -> Either e a
-ma !? e = maybe (Left e) Right ma
+  case M.lookup method methods of
+    Nothing -> return $ Left $ MethodNotFound method
+    Just (Method f expectedArgs) -> do
+      case f args of
+        Nothing -> return $ Left $ MismatchedArgs expectedArgs
+        Just io -> Right <$> io
 
 ----------------------------------------
 -- Program
@@ -51,21 +56,28 @@ main = do
   let custom :: ([Int], Char, String) -> Int
       custom ([_, _, code], _, _) = code
 
+  counter <- newIORef 0
+  let faa :: () -> IO Int
+      faa () = atomicModifyIORef' counter (\x -> (x + 1, x))
+
   let methods =
         M.fromList
-          [ ("even", mkMethod even_)
-          , ("reverse", mkMethod reverse_)
-          , ("custom", mkMethod custom)
+          [ ("even", mkMethod $ pure . even_)
+          , ("reverse", mkMethod $ pure . reverse_)
+          , ("faa", mkMethod faa)
+          , ("custom", mkMethod $ pure . custom)
           ]
   forM_
     [ ("even", show (1 :: Int))
     , ("reverse", show "hello")
     , ("custom", show ([1, 2, 3], 'c', "FOO"))
+    , ("faa", show ())
+    , ("faa", show ())
     , ("not-found", show ())
     , ("even", show "boo!")
     ]
     $ \rpc -> do
-      let result = runRpc methods rpc
+      result <- runRpc methods rpc
       putStr $ "(" ++ fst rpc ++ ") "
       case result of
         Right v -> putStrLn $ "Result: " ++ v
