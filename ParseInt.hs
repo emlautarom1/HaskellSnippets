9802482e3b5864883f6ca250154e3f60df7e722b
Lautaro Emanuel <emlautarom1@gmail.com>
2024-03-07 12:23:47 -0300

    Add better 'parseInt'

--- ParseInt.hs
+++ ParseInt.hs
@@ -1,10 +1,11 @@
 module ParseInt
-  ( parseInt,
-    unsafeParseInt,
+  ( parseInt
+  , unsafeParseInt
   )
 where
 
 import Data.Char as Char (digitToInt, isDigit)
+import Data.Foldable (foldl')
 
 safeDigitToInt :: Char -> Maybe Int
 safeDigitToInt c =
@@ -19,9 +20,19 @@ parseInt str = do
   digits <- reverseDigits
   pure $ foldr (\(idx, n) res -> res + n * 10 ^ idx) 0 (zip [0 ..] digits)
   where
-    reverseDigits = sequence $ safeDigitToInt <$> reverse str
+    reverseDigits = mapM safeDigitToInt (reverse str)
+
+parseInt' :: String -> Maybe Int
+parseInt' "" = Nothing
+parseInt' ('-' : n) = negate <$> parseInt n
+parseInt' str = do
+  digits <- traverse safeDigitToInt str
+  return $ foldl' (\num digit -> num * 10 + digit) 0 digits
 
 unsafeParseInt :: String -> Int
 unsafeParseInt str = foldr (\(idx, n) res -> res + n * 10 ^ idx) 0 (zip [0 ..] reverseDigits)
   where
     reverseDigits = Char.digitToInt <$> reverse str
+
+-- >>> parseInt' "-1"
+-- Just (-1)
