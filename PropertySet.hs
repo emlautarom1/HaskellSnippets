58f78d51c46c9eb1edcc66e0e2d94db5c9fa3196
Lautaro Emanuel <emlautarom1@gmail.com>
2024-03-07 13:49:06 -0300

    Improve docs

--- PropertySet.hs
+++ PropertySet.hs
@@ -1,34 +1,34 @@
 {-# OPTIONS_GHC -Wall #-}
 
--- | Property-based (possible infinite) sets, heavily inspired by 'https://www.youtube.com/watch?v=Y5rPHZaUakg'
--- I tried to mimic the interface of 'Data.Set' as closely as possible, but some operations
+-- | Property-based (possible infinite) sets, heavily inspired by @https://www.youtube.com/watch?v=Y5rPHZaUakg@
+-- It tries to mimic the interface of 'Data.Set' as closely as possible, but some operations
 -- are impossible, given the nature of the sets (ex. 'elems' can't be defined).
 module PropertySet
   ( -- * Set type
-    Set,
-    Predicate,
+    Set
+  , Predicate
 
     -- * Query
-    member,
-    notMember,
+  , member
+  , notMember
 
     -- * Construction
-    empty,
-    insert,
-    insertAll,
-    delete,
-    deleteAll,
-    singleton,
-    fromList,
-    fromPredicate,
+  , empty
+  , insert
+  , insertAll
+  , delete
+  , deleteAll
+  , singleton
+  , fromList
+  , fromPredicate
 
     -- * Combine
-    union,
-    difference,
-    intersection,
+  , union
+  , difference
+  , intersection
 
     -- * Operators
-    (\\),
+  , (\\)
   )
 where
 
@@ -59,6 +59,11 @@ empty :: Set a
 empty = Empty
 
 -- | Is a given element in the set?
+--
+-- ==== __Examples__
+--
+-- >>> member 5 $ empty
+-- False
 member :: a -> Set a -> Bool
 member _ Empty = False
 member a (set `WithOperation` op) = case op of
@@ -71,7 +76,12 @@ notMember a s = not $ member a s
 
 -- | Insert a single element in a set.
 -- Insertion of an element already on the set is a no-op.
-insert :: Eq a => a -> Set a -> Set a
+--
+-- ==== __Examples__
+--
+-- >>> member 5 $ insert 5 $ empty
+-- True
+insert :: (Eq a) => a -> Set a -> Set a
 insert a s
   | notMember a s = s `WithOperation` Insert (== a)
   | otherwise = s
@@ -82,24 +92,39 @@ insertAll p s = s `WithOperation` Insert p
 
 -- | Delete a single element from a set.
 -- Deletion of an element not in the set is a no-op.
-delete :: Eq a => a -> Set a -> Set a
+--
+-- ==== __Examples__
+--
+-- >>> member 5 $ delete 5 $ insert 5 $ empty
+-- False
+delete :: (Eq a) => a -> Set a -> Set a
 delete a s
   | member a s = s `WithOperation` Delete (== a)
   | otherwise = s
 
 -- | Delete all the elements that satisfy a given @Predicate@ in a set.
+--
+-- ==== __Examples__
+--
+-- >>> member 10 $ deleteAll (\x -> x `mod` 5 == 0) $ fromPredicate even
+-- False
 deleteAll :: Predicate a -> Set a -> Set a
 deleteAll p s = s `WithOperation` Delete p
 
 -- | Create a set with a single element.
-singleton :: Eq a => a -> Set a
+singleton :: (Eq a) => a -> Set a
 singleton a = insertAll (== a) empty
 
 -- | Create a set with all the elements from a list.
-fromList :: Eq a => [a] -> Set a
+fromList :: (Eq a) => [a] -> Set a
 fromList l = insertAll (`elem` l) empty
 
 -- | Create a set with all the elements that satisfy a given @Predicate@.
+--
+-- ==== __Examples__
+--
+-- >>> member 10 $ fromPredicate (\x -> x `mod` 5 == 0)
+-- True
 fromPredicate :: Predicate a -> Set a
 fromPredicate p = insertAll p empty
 
@@ -118,21 +143,3 @@ intersection s1 s2 = empty `WithOperation` Insert (\x -> x `member` s1 && x `mem
 -- | See 'difference'
 (\\) :: Set a -> Set a -> Set a
 (\\) = difference
-
--- | Examples
--- __Simple examples__
--- >>> member 5 $ empty
--- False
-
--- >>> member 5 $ insert 5 $ empty
--- True
-
--- >>> member 5 $ delete 5 $ insert 5 $ empty
--- False
-
--- __Examples with properties__
--- >>> member 10 $ fromPredicate (\x -> x `mod` 5 == 0)
--- True
-
--- >>> member 10 $ deleteAll (\x -> x `mod` 5 == 0) $ fromPredicate even
--- False
