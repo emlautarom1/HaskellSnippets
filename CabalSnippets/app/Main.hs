38995fd8e404c6148cdc9f6e4862196b9d1b50b5
Lautaro Emanuel <emlautarom1@gmail.com>
2025-03-11 16:59:16 -0300

    Remove wrong imports

--- CabalSnippets/app/Main.hs
+++ CabalSnippets/app/Main.hs
@@ -2,9 +2,7 @@ module Main where
 
 -- import qualified DependencyInjection
 -- import qualified EffectfulLogger
--- import qualified LensesLib
 -- import qualified LensesMain
--- import qualified Math
 -- import qualified Merkle
 -- import qualified SystemInfo
 -- import qualified TaglessFinal
